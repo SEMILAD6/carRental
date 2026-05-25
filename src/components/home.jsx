88aec9a8edfedb6086a9e5331a0619d13127aa4d
SEMILORE <mac@Semilores-MacBook-Air.local>
2026-05-25 17:07:46 +0100

    update

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { getAllCars } from "../api/reservations";
+import { getAllCars } from "../Api/reservations";
 import "./home.css";
 
 function Home() {
