a1e5ffceeeb5676c7bafec75a8d9d733d1df9ae9
SEMILORE <mac@Semilores-MacBook-Air.local>
2026-05-25 16:39:06 +0100

    update

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ import reservationRoutes from "./routes/reservations.js";
 
 const app = express();
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: ["http://localhost:5173", "https://carrental-yrah.onrender.com"], credentials: true }));
 app.use(express.json());
 
 // Routes
--- src/Api/reservations.js
+++ src/Api/reservations.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:8000/api";
+const BASE_URL = "https://carrental-yrah.onrender.com";
 
 export const getAvailableCars = async ({ pickupDate, returnDate, type }) => {
   const params = new URLSearchParams({ pickupDate, returnDate });
