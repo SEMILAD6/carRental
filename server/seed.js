164117802cc3ee7da98558aed3c451549935e72e
SEMILORE <mac@Semilores-MacBook-Air.local>
2026-05-25 22:41:41 +0100

    update

--- server/seed.js
+++ server/seed.js
@@ -25,7 +25,7 @@ await Car.insertMany([
   { name: "Porsche Cayenne", type: "Luxury", seats: 5, pricePerDay: 450000, totalUnits: 2, image: "porsche-cayenne.jpg" },
   { name: "Ferrari SF90", type: "Exotic", seats: 2, pricePerDay: 1500000, totalUnits: 1, image: "ferrari-sf90.jpg" },
   { name: "Lamborghini Huracán", type: "Exotic", seats: 2, pricePerDay: 1700000, totalUnits: 1, image: "lamborghini-huracan.jpg" },
-  { name: "Rolls Royce Ghost", type: "Exotic", seats: 4, pricePerDay: 2000000, totalUnits: 1, image: "rolls-royce-ghost.jpg" },
+  { name: "Rolls Royce Ghost", type: "Exotic", seats: 4, pricePerDay: 2000000, totalUnits: 1, image: "https://images.unsplash.com/photo-1625510872834-7db6c4273870?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fHJvbGxzJTIwcm95Y2UlMjBnaG9zdHxlbnwwfHwwfHx8MA%3D%3D" },
 ]);
 
 console.log("URI:", process.env.MONGO_URI);
