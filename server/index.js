28d4e2b56ad955c1468033bc2a34eaee92ccee1b
SEMILORE <mac@Semilores-MacBook-Air.local>
2026-06-20 12:47:43 +0100

    Permission Edit

--- server/index.js
+++ server/index.js
@@ -13,8 +13,22 @@ import reservationRoutes from "./routes/reservations.js";
 
 
 const app = express();
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://car-rental-eight-pink.vercel.app"
+];
 
-app.use(cors({ origin: "https://car-rental-eight-pink.vercel.app", credentials: true }));
+
+app.use(cors({ 
+    origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true
+}));
 app.use(express.json());
 
 // Routes
